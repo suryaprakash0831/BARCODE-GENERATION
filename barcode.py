665a419a67e799fbbb3a4659e510d7b9a66092a5
SURYAPRAKASH S <suryaprakash0831@gmail.com>
2019-10-20 16:51:37 +0530

    Update barcode.py

--- barcode.py
+++ barcode.py
@@ -4,5 +4,5 @@ A = barcode.get_barcode_class('ean13')
 a = A(input('enter the 12 number which has to converted into barcode: '), writer=ImageWriter())#in this format we have to give 12 digits
 a.save(input("enter the file name :"))
 #we can generate all types of barcode by just changing string in .get_barcode_class('here we have to change') by below list
-#['code39', 'code128', 'ean', 'ean13', 'ean8', 'gs1', 'gtin', 'isbn', 'isbn10', 'isbn13', 'issn', 'jan', 'pzn', 'upc', 'upca']
+#'code39', 'code128', 'ean', 'ean13', 'ean8', 'gs1', 'gtin', 'isbn', 'isbn10', 'isbn13', 'issn', 'jan', 'pzn', 'upc', 'upca'
 #we have to install barcode modlue by giving command:' pip install python-barcode'
